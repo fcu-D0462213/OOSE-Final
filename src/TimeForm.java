84cbd7beb55dd3a7541ef599003a80dca5c2f677
王喆 <654391654@qq.com>
2019-01-07 17:31:01 +0800

    添加TimeMange GUI

---
+++ src/TimeForm.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+public class TimeForm {
+    private JPanel Panel1;
+    private JPanel Panel2;
+    private JButton OutputButton;
+    private JTextField NameText;
+    private JLabel NameLabel;
+    private JTextField BeginTimeText;
+    private JLabel BeginTimeLabel;
+    private JLabel OverTimeLabel;
+    private JTextField OverTimeText;
+    private JButton InitialInformationButton;
+    private JLabel InitialInformationOutputLabel;
+    private JLabel ItemNameLabel;
+    private JTextField ItemNameOutputText;
+    private JLabel ItemCostLabel;
+    private JTextField ItemCostOutputText;
+    private JLabel ItemCategoryLabel;
+    private JRadioButton CategoryRadioButton1;
+    private JRadioButton CategoryRadioButton2;
+    private JRadioButton CategoryRadioButton3;
+    private JLabel ItemPriorityLabel;
+    private JRadioButton PriorityRadioButton1;
+    private JRadioButton PriorityRadioButton2;
+    private JRadioButton PriorityRadioButton3;
+    private JButton ItemOutputButton;
+    private JLabel ItemOutputLabel;
+    private JButton ConfirmButton;
+    private JButton FinishButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("TimeForm");
+        frame.setContentPane(new TimeForm().Panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
